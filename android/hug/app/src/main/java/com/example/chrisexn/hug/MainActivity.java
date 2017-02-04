38918968cc88651a4ced4e04774985a3f2267e31
Dexeux <cgaboury@live.com>
2017-02-04 00:30:05 -0500

    Initial project

---
+++ android/hug/app/src/main/java/com/example/chrisexn/hug/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.chrisexn.hug;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chrisexn on 2/4/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
