e9d7402a9678067bcf85420225f358751ababbaa
Dexeux <cgaboury@live.com>
2017-02-04 12:01:47 -0500

    Close to success

---
+++ android/hug/app/src/main/java/com/example/chrisexn/hug/GettingHug.java
@@ -0,0 +1,12 @@
+package com.example.chrisexn.hug;
+
+/**
+ * Created by chrisexn on 2/4/2017.
+ */
+
+public class GettingHug implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
